2a099ff7bc8150115dc6cf8d4188b9cbf1e93d03
Mauricio Dellagnese <mauricio.dellagnese@gmail.com>
2017-08-01 23:42:50 -0300

    Added oen item to list view

--- BalaioCulturalNew/BalaioCulturalNew/ViewModels/Feed/FeedPageViewModel.cs
+++ BalaioCulturalNew/BalaioCulturalNew/ViewModels/Feed/FeedPageViewModel.cs
@@ -3,16 +3,25 @@ using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace BalaioCulturalNew.ViewModels.Feed
 {
-	public class FeedPageViewModel : BaseViewModel
-	{
+    public class FeedPageViewModel : BaseViewModel
+    {
         public FeedPageViewModel(INavigationService navigationService) : base(navigationService)
         {
-
+            Events = new ObservableCollection<string> { "Teste" };
         }
-        
-	}
+
+        private ObservableCollection<String> _events;
+
+        public ObservableCollection<string> Events
+        {
+            get { return _events; }
+            set { SetProperty(ref _events, value); }
+        }
+
+    }
 }
