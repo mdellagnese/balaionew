74eecf7b1d2bbade24e8626d8bc79ee820c6a7a0
Mauricio Dellagnese <mauricio.dellagnese@gmail.com>
2017-08-07 01:50:34 -0300

    Layout Enhancements

--- BalaioCulturalNew/BalaioCulturalNew/ViewModels/Login/EntryPageViewModel.cs
+++ BalaioCulturalNew/BalaioCulturalNew/ViewModels/Login/EntryPageViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Prism.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,17 @@ namespace BalaioCulturalNew.ViewModels.Login
 	{
         public DelegateCommand NavigateToMainMenuCommand { get; private set; }
         public DelegateCommand FacebookLoginCommand { get; private set; }
-        
-        public EntryPageViewModel(INavigationService navigationService) : base(navigationService)
+        public DelegateCommand NavigateToRegisterPageCommand { get; private set; }
+        public DelegateCommand NavigateToForgotPageCommand { get; private set; }
+
+        public EntryPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService) : base(navigationService, pageDialogService)
         {
             //Adicionar ObservesProperty para o preenchimento do form
             NavigateToMainMenuCommand = new DelegateCommand(() => _navigationService.NavigateAsync("app:///MainPage"));
 
             FacebookLoginCommand = new DelegateCommand(() => _navigationService.NavigateAsync("FacebookLoginPage",null,true));
+            NavigateToRegisterPageCommand = new DelegateCommand(() => _navigationService.NavigateAsync("RegisterPage"));
+            NavigateToForgotPageCommand = new DelegateCommand(async () => await _pageDialogService.DisplayAlertAsync("Aguarde", "Estamos trabalhando nisso :)", "OK"));
         }
 	}
 }
