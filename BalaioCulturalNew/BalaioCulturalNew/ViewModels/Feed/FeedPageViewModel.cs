2e5dbe506bfca27e3f0516392e3aa6fa807b9e32
Mauricio Dellagnese <mauricio.dellagnese@gmail.com>
2017-08-15 11:40:59 -0300

    Added ItemTapped Behavior. Pending EventArgs

--- BalaioCulturalNew/BalaioCulturalNew/ViewModels/Feed/FeedPageViewModel.cs
+++ BalaioCulturalNew/BalaioCulturalNew/ViewModels/Feed/FeedPageViewModel.cs
@@ -4,15 +4,24 @@ using Prism.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 
 namespace BalaioCulturalNew.ViewModels.Feed
 {
     public class FeedPageViewModel : BaseViewModel
     {
+        public DelegateCommand EventSelectedCommand { get; private set; }
+
         public FeedPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Events = new ObservableCollection<string> { "Teste", "Teste 2", "Teste 2" };
+            EventSelectedCommand = new DelegateCommand(SelectEvent);
+        }
+
+        private void SelectEvent()
+        {
+            Debug.WriteLine("Evento Selecionado");
         }
 
         private ObservableCollection<String> _events;
