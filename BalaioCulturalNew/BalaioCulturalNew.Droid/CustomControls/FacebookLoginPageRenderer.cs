5a02793e9ed10f670c350d35a95312fa06a0ed60
Mauricio Dellagnese <mauricio.dellagnese@gmail.com>
2017-08-27 21:51:46 -0300

    Added facebook info for Android

--- BalaioCulturalNew/BalaioCulturalNew.Droid/CustomControls/FacebookLoginPageRenderer.cs
+++ BalaioCulturalNew/BalaioCulturalNew.Droid/CustomControls/FacebookLoginPageRenderer.cs
@@ -5,7 +5,8 @@ using BalaioCulturalNew.Views.Login;
 using Xamarin.Auth;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
-using Newtonsoft.Json.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 
 [assembly: ExportRenderer(typeof(FacebookLoginPage), typeof(FacebookLoginPageRenderer))]
@@ -20,51 +21,71 @@ namespace BalaioCulturalNew.Droid.CustomControls
 
             var auth = new OAuth2Authenticator(
                     clientId: "1834003460194581",
-                    scope: "public_profile+email",
+                    scope: "",
                     authorizeUrl: new Uri("https://m.facebook.com/dialog/oauth/"),
                     redirectUrl: new Uri("http://www.facebook.com/connect/login_success.html")
                 );
 
-            auth.Completed += async (sender, eventArgs) =>
+            auth.Completed += FacebookLoginasync;
+
+            activity.StartActivity(auth.GetUI(activity));
+        }
+
+        private async void FacebookLoginasync(object sender, AuthenticatorCompletedEventArgs eventArgs)
+        {
+            
+            // We presented the UI, so it's up to us to dimiss it on iOS.
+            //DismissViewController(true, null);
+
+            if (eventArgs.IsAuthenticated)
             {
-                // We presented the UI, so it's up to us to dimiss it on iOS.
-                //DismissViewController(true, null);
+                // Use eventArgs.Account to do wonderful things
+                var userAccount = eventArgs.Account;
+                var accessToken = userAccount.Properties["access_token"];
 
-                if (eventArgs.IsAuthenticated)
+                if (MainActivity.NeedRegistration == true)
                 {
-                    // Use eventArgs.Account to do wonderful things
-                    var userInfo = eventArgs.Account;
-                    var accessToken = userInfo.Properties["access_token"];
-
-                    if (MainActivity.NeedRegistration == true)
+                    try
                     {
                         //Get facebook Information
                         var graphRequest = new OAuth2Request(
                             "GET",
-                            new Uri("https://graph.facebook.com/me?fields=picture,email,gender"),
+                            new Uri("https://graph.facebook.com/me?fields=email,picture"),
                             null,
-                            userInfo
+                            userAccount
                         );
 
-                        var userDetails = await graphRequest.GetResponseAsync();
-                        Console.WriteLine(userDetails);
-                    }
+                        var response = await graphRequest.GetResponseAsync();
+                        var userData = JsonConvert.SerializeObject(response.GetResponseText());
 
-                    //Save the API Token - We need a new request to Balio API to get the token
-                    App.Current.Properties["fb_access_token"] = accessToken;
-                    
-                    //Navigate
+                        Console.WriteLine("Dados");
+                        //Save the Users details
+                    }
+                    catch (Exception e)
+                    {
+                        throw e;
+                    }
+                }
+
+                //Save the API Token - We need a new request to Balio API to get the token
+                App.Current.Properties["fb_access_token"] = accessToken;
+
+                //Navigate
+                try
+                {
                     (App.Current as App).SuccessfulLoginAction();
                 }
-                else
+                catch (Exception e)
                 {
-                    // The user cancelled
-                    (App.Current as App).GoBack.Invoke();
+                    throw e;
                 }
-            };
-
-            activity.StartActivity(auth.GetUI(activity));
+            }
+            else
+            {
+                // The user cancelled
+                (App.Current as App).GoBack.Invoke();
+            }
+            
         }
-        
     }
 }
