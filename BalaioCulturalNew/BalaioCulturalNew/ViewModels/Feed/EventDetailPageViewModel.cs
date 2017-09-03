065724c6a0e42d78904d2b0bed2b9344bce7d33b
DESKTOP-MD\Mauricio <mauricio.dellagnese@gmail.com>
2017-09-03 01:02:12 -0300

    Sync

--- BalaioCulturalNew/BalaioCulturalNew/ViewModels/Feed/EventDetailPageViewModel.cs
+++ BalaioCulturalNew/BalaioCulturalNew/ViewModels/Feed/EventDetailPageViewModel.cs
@@ -14,27 +14,34 @@ namespace BalaioCulturalNew.ViewModels.Feed
         public DelegateCommand RouteToEventCommand { get; private set; }
         public DelegateCommand BuyTicketToEventCommand { get; private set; }
         public DelegateCommand ShareEventCommand { get; private set; }
+        public DelegateCommand AddToBalaioEventCommand { get; private set; }
 
         public EventDetailPageViewModel(INavigationService navigationService, IPageDialogService pageDialogService) : base(navigationService, pageDialogService)
         {
             RouteToEventCommand = new DelegateCommand(GetRouteToEvent);
             BuyTicketToEventCommand = new DelegateCommand(BuyTicketToEvent);
             ShareEventCommand = new DelegateCommand(ShareEvent);
+            AddToBalaioEventCommand = new DelegateCommand(AddToBalaioEvent);
+        }
+
+        private void AddToBalaioEvent()
+        {
+            Debug.WriteLine("Chamar API passando o id do evento.");
         }
 
         private void BuyTicketToEvent()
         {
-            Debug.WriteLine("Here");
+            Debug.WriteLine("Chamar o navegador com a URL");
         }
 
         private void ShareEvent()
         {
-            Debug.WriteLine("Here");
+            Debug.WriteLine("Compartilhar Evento, como aonde?");
         }
 
         private void GetRouteToEvent()
         {
-            Debug.WriteLine("Here");
+            Debug.WriteLine("Abrir no maps.");
         }
     }
 }
